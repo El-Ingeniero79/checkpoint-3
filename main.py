ee53c0bba545197954f80a63e099bc13484d53f5
El-Ingeniero79 <150141876+El-Ingeniero79@users.noreply.github.com>
2024-02-23 10:38:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+# exercise 1
+my_string = "hola que tal"
+my_number = 5
+my_list = [1, 2, 3]
+my_boolean = True
+
+# exercise 2
+my_index = my_string [0:3]
+print(my_index)
+
+# exercise 3
+my_second_index = my_list [0]
+print (my_second_index)
+
+# exercise 4
+new_number = my_number + 10
+print (new_number)
+
+# exercise 5
+last_element = my_list [-1]
+print (last_element)
+
+# exercise 6
+names = "harry,alex,susie,jared,gail,conner"
+list_names = names.split(",")
+print (list_names)
+
+# exercise 7
+first_word = my_string[0:4]
+uppercase_word = first_word.upper()
+my_new_string = my_string.replace("hola", uppercase_word)
+print (my_new_string)
+
+# exercise 8
+last_sentence = f"mi numero favorito es el {my_number}"
+print (last_sentence)
+
+# exercise 9
+print ("hello world")
